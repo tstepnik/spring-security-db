6cdd7fd3e578cf7325f57b64d8171446300aa92c
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-22 19:58:30 +0100

    essential files

---
+++ src/main/java/pl/javastart/SpringSecurityDbApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDbApplication.class, args);
+    }
+
+}
